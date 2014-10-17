db7a315067bd1adde6201fc83af0df338094e03b
clemtoy <cmich@hotmail.fr>
2014-10-17 17:20:34 +0200

    Criteria

--- criteria.py
+++ criteria.py
@@ -1,9 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -O
 # -*-coding:utf-8 -*
 
+import math
+
 class Criteria:
-	__init__(self, weighting, values, rule, influence):
-		self.weighting = weighting
-		self.values = values # [vmin]
-		self.rule = rule # min, =, max
-		self.influence = influence
+	def __init__(self, weight, maxVal, rule, influence):
+		self.weight = weight
+		self.maxVal = maxVal
+		self.rule = rule # minimize, egalize, maximize
+		self.influence = influence # [fonction, parametres...]
+
+	def score(self, averageInAccomodation, valueOfFamily):
+		return 1-influence({"valueOfFamily":valueOfFamily, "averageInAccomodation":averageInAccomodation}, "maxVal":maxVal)
+
+
+#rules
+	@staticmethod
+	def egalize(cls, values):
+		return math.fabs(values["valueOfFamily"]-values["averageInAccomodation"]) / values["maxVal"]
+
+	@staticmethod
+	def inegalize(cls, values):
+		return 1-math.fabs(values["valueOfFamily"]-values["averageInAccomodation"]) / values["maxVal"]
+
+	@staticmethod
+	def minimize(cls, values):
+		return (values["maxVal"] - values["valueOfFamily"]) / values["maxVal"]
+
+	@staticmethod
+	def maximize(cls, values):
+		return (values["valueOfFamily"] - values["maxVal"]) / values["maxVal"]
+
+#influences
+	@staticmethod
+	def exp(dec, value):
+		return math.exp(dec * value)
--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+
+from criteria import *
+
+#Définition des critères
+criterias = {}
+criterias["type"] 	= Criteria(0.5, [1,2], Criteria.egalize, [Criteria.exp, -1])
+criterias["income"] = Criteria(0.5, [20,50], Criteria.maximize, [Criteria.exp, -1])
